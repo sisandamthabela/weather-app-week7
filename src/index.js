254b8b8ac83f963aa4464dbe5cb9a81d4f89f341
sisandamthabela <mthabelasisanda@gmail.com>
2024-04-04 09:16:09 +0200

    api integration

--- src/index.js
+++ src/index.js
@@ -22,20 +22,37 @@ function updateTime() {
 setInterval(updateTime, 1000);
 
 function updateWeather(response) {
+  let currentLocation = document.querySelector("#current-city");
+  let temp = document.querySelector("#current-temp");
   let descriptionElement = document.querySelector("#condition");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let feelsLike = document.querySelector("#feels-like");
+  let icon = document.querySelector("#icon");
+
+  currentLocation.innerHTML = `${response.data.city}`;
+  temp.innerHTML = `${Math.round(response.data.temperature.current)}`;
   descriptionElement.innerHTML = response.data.condition.description;
   console.log(response);
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  feelsLike.innerHTML = `Feels like ${Math.round(
+    response.data.temperature.feels_like
+  )}°`;
+  icon.innerHTML = `<img src ="${response.data.condition.icon_url}"/>`;
+}
+function searchCity(city) {
+  let apiKey = "7a60d36485to64ac27f9bab0d1cc4ca3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let location = searchInput.value;
-
-  let key = "7a60d36485to64ac27f9bab0d1cc4ca3";
-  let url = `https://api.shecodes.io/weather/v1/current?query=${location}&key=${key}&units=metric`;
-  axios.get(url).then(updateWeather);
+  searchCity(searchInput.value);
 }
-let searchForm = document.querySelector("#search-button");
+
+let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
-searchLocation = "Paris";
+searchCity("Ixopo");
